743c4397c3f530a511c3aad29de51e4d8b3e5e61
NaveenPensia70 <pensianaveen70@gmail.com>
2024-08-06 13:51:27 +0530

    feat: add 3 buttons for color. Added 'shading-btn','eraser-btn' and 'rainbow-btn'. As I added 3 different buttons performing coloring boxes.'shading-btn' darkens the element on which it is hovered, 'eraser-btn' change back to white color and rainbow color gives different color on hovering.

--- script.js
+++ script.js
@@ -1,40 +1,82 @@
-var boxContainer=document.querySelector(".container");
-var gridBoxes=document.querySelector(".grid-boxes");
-var btn=document.querySelector(".btn");
+var boxContainer = document.querySelector(".container");
+var gridBoxes = document.querySelector(".grid-boxes");
+var btn = document.querySelector(".btn");
+var shadeBtn = document.querySelector(".shading-btn");
+var eraserBtn=document.querySelector(".eraser-btn");
+var rainbowBtn=document.querySelector(".rainbow-btn");
+var newBoxArray = [];
 var inputBox;
 
-btn.addEventListener("click",runPrompt);
-function runPrompt(){
-inputBox=prompt("Enter the number of squares per side: ");
-if(inputBox!=null)
-    {
-         //We need to empty the gridBoxes before filling it again
-        while(gridBoxes.firstChild)
-            {
-                gridBoxes.firstChild.remove();
-            } 
-            //Setting custom css property for each side of gridBoxes 
-            gridBoxes.style.setProperty("--each-side",inputBox);   
-         for(var i=0;i<inputBox;i++)
-             { 
-                for(var j=0;j<inputBox;j++) 
-                {                   
-                var newBox=document.createElement("div");
-                newBox.classList.add("new-box");               
+btn.addEventListener("click", runPrompt);
+shadeBtn.addEventListener("click", runShading); 
+eraserBtn.addEventListener("click",runErasing);
+rainbowBtn.addEventListener("click",runRainbow);
+
+function runPrompt() {
+    inputBox = prompt("Enter the number of squares per side: ");
+    if (inputBox != null) {
+        // We need to empty the gridBoxes before filling it again
+        while (gridBoxes.firstChild) {
+            gridBoxes.firstChild.remove();
+        }
+        newBoxArray = [];
+        // Setting custom css property for each side of gridBoxes
+        gridBoxes.style.setProperty("--each-side", inputBox);
+
+        for (var i = 0; i < inputBox; i++) {
+            for (var j = 0; j < inputBox; j++) {
+                var newBox = document.createElement("div");
+                newBox.classList.add("new-box");
                 gridBoxes.appendChild(newBox);
-                newBox.textContent=j + 1; 
-                newBox.textContent= `${i},${j}`;          
-                } 
-                var nextLine=document.createElement("br");
-                gridBoxes.appendChild(nextLine);
-                }
-                }
-     }
+                newBox.textContent = `${i},${j}`;
+                newBox.setAttribute("darkness",255)
+                newBoxArray.push(newBox);
+            }
+        }
+    }
+}
 
+function runShading() {
+    newBoxArray.forEach(function(newo) {
+        newo.addEventListener("mouseover", shading);
+    });
+}
 
+function shading(event) {
+    let temp=event.target;
+    let currentDarkness=parseInt(temp.getAttribute("darkness"));
+    if(currentDarkness>0)
+    {
+        currentDarkness-=25;
+        temp.style.backgroundColor=`rgb(${currentDarkness},${currentDarkness},${currentDarkness})`;
+        temp.setAttribute("darkness",currentDarkness);
+    }
+}
 
+function runErasing(event)
+{
+    newBoxArray.forEach(function(newErase){
+        newErase.addEventListener("mouseover",erasing);
+    });
+}
 
+function erasing(event){
+    event.target.style.backgroundColor="rgb(255,255,255)";
+}
 
+function runRainbow()
+{
+    newBoxArray.forEach(function(newRainbow){
+        newRainbow.addEventListener("mouseover",rainbow);
+    });
+}
 
-
-
+function randomNumber(){
+    let min=0;
+    let max=255;
+    let randomColor;
+    return randomColor=Math.floor(Math.random()*(max-min+1)+min);
+}
+function rainbow(event){
+    event.target.style.backgroundColor=`rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+}
